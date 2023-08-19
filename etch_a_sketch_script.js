1d3268f994156632a8472a0c736fad4ce5584778
Zach1281 <zachary_romero@yahoo.com>
2023-08-19 12:39:21 -0700

    Complete etch-a-sketch prototype
    Button resets the grid and mousing over grid changes color

--- etch_a_sketch_script.js
+++ etch_a_sketch_script.js
@@ -1,15 +1,39 @@
-const container = document.querySelector('#drawingSpace');
+createGrid();
 
-
-for(let i = 0; i < 16; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
-    for(let j = 0; j < 16; j++){
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.cssText = "";
-        cell.textContent = "Cells";
-        row.appendChild(cell);
+function createGrid() {
+    const container = document.querySelector('#drawingSpace');
+    for(let i = 0; i < 16; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for(let j = 0; j < 16; j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.addEventListener("mouseover", 
+            (event) => {
+                event.target.style.cssText = "background-color: white";
+            },
+            false
+            );
+            cell.style.cssText = "";
+            row.appendChild(cell);
+        }
     }
 }
+
+function deleteGrid() {
+    const row = document.querySelectorAll('.row');
+    for(let i = 0; i < row.length; i++){
+        row[i].remove(row[i]);
+    }
+}
+
+
+
+const promptBtn = document.querySelector('.prompt');
+promptBtn.addEventListener('click',
+    () => {
+        deleteGrid();
+        createGrid();
+    });
+
